952727858892a14a81f8ccc0e72e3b0d6aa48a22
emmanoel-amunik <emmanoelamunik@hotmail.com>
2024-01-08 22:03:59 -0300

    02. Operacoes

--- Conjuntos.py
+++ Conjuntos.py
@@ -12,10 +12,8 @@ print(watched)
 for user in set(watched):
     print(user)
 
-
 data_science_users = set(data_science_users)
 machine_learning_users = set(machine_learning_users)
-
 
 # "|" means or
 print(data_science_users | machine_learning_users)
@@ -32,3 +30,30 @@ print(data_science_users ^ machine_learning_users)
 did = data_science_users - machine_learning_users
 print(15 in did)
 print(23 in did)
+
+print("...........................")
+
+users = {1, 5, 76, 34, 52, 13, 17}
+print(len(users))
+
+users.add(13)
+print(len(users))
+
+users.add(765)
+print(len(users))
+print(users)
+
+users = frozenset(users)  # you can't add anymore
+
+print(type(users))
+# users.add(29)
+print(users)
+
+phrase = ("Allegri's haunting Miserere is famous for both its ethereal beauty "
+          "and for the mystery surrounding its composition. It is written "
+          "for 2 choirs, who alternate phrases and then unite for a final "
+          "resolution.")
+
+phrase.split(' ')
+phrase = set(phrase.split())
+print(phrase)
